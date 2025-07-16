3e95ac027f0e8428379d04d5842f18c442bc048f
lucaskraus <lucas-kraus@hotmail.com>
2025-07-16 20:31:46 -0300

    Refactor server initialization by moving it to app.ts and enhance environment variable validation with error handling in env/index.ts

--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,12 @@ app.register(usersRoutes);
 app.register(mealsRoutes, {
   prefix: 'meals',
 });
+
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP Server Running! 🚀');
+  });
--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -2,9 +2,15 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 export const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   PORT: z.coerce.number().default(3333),
   JWT_SECRET: z.string(),
 });
 
-export const env = envSchema.parse(process.env);
+const { error, data } = envSchema.safeParse(process.env);
+
+if (error) {
+  console.error('Invalid environment variables. Please check .env file. 🚨', error);
+  process.exit(1);
+}
+
+export const env = data;
